323fef67929d54050ee1c9f81752e22d080aa0c3
Alok78 <alokkaushalrbl@gmail.com>
2023-01-16 13:25:28 +0530

    Config Server

---
+++ src/test/java/com/pratice/configserver/CinfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratice.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
